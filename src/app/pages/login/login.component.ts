26577f502876fad8979585be8ddb10b3fb4b8319
L0G1C06 <justgithub70@gmail.com>
2024-04-03 15:00:47 -0300

    login implemented

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  login(){
+    if (this.user === 'admin' && this.password === '1234'){
+      this.router.navigate(['/app'])
+    } else {
+      alert('User or password are incorrect!')
+      this.user = '';
+      this.password = '';
+    }
+  }
 }
