864f8c2f4c3c75849be1c5bf9dcf47a17a66b4b6
L0G1C06 <justgithub70@gmail.com>
2024-04-04 11:32:01 -0300

    feat: add communication with database server to save signup data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -12,9 +13,9 @@ export class SignupComponent {
   user: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
-  signup() {
+  async signup() {
     if (this.email && this.user && this.password && this.password.length >= 8) {
       const newUser = {
         email: this.email,
@@ -22,19 +23,32 @@ export class SignupComponent {
         password: this.password
       };
 
-      this.createNewUser(newUser).then(() => {
+      try {
+        await this.createNewUser(newUser);
         console.log('New User:', newUser);
         alert('User successfully registered!');
         this.router.navigate(['/login']); 
-      }).catch(error => {
+      } catch (error) {
         console.error('Error when registering user:', error);
         alert('There was an error registering the user. Please try again.');
-      });
+      }
     } else {
       alert('Please fill in all the fields correctly.');
     }
   }
 
   private async createNewUser(newUser: any): Promise<void> {
+    const url = 'http://localhost:8000/api/register';
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    try {
+      await this.http.post(url, newUser, { headers }).toPromise();
+      return; // Explicitly return void here
+    } catch (error) {
+      throw error;
+    }
   }
 }
