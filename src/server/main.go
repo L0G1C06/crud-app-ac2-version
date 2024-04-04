8cb097893ad535acaf2d94fee3537363a2f15b08
L0G1C06 <justgithub70@gmail.com>
2024-04-04 11:31:11 -0300

    feature: add golang server to comunicate with database

---
+++ src/server/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/rs/cors"
+)
+
+type User struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "/home/weber/projects/faculdade/crud-app/src/server/database/signup.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	createUsersTable(db)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var user User
+		if err := DecodeJSONBody(w, r, &user); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err := saveUser(db, user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "User successfuly registered!")
+	})
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	})
+	handler := c.Handler(mux)
+	log.Println("API server created and listening in :8000")
+	log.Fatal(http.ListenAndServe(":8000", handler))
+}
+
+func createUsersTable(db *sql.DB) {
+	sqlStmt := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			email TEXT UNIQUE NOT NULL,
+			username TEXT UNIQUE NOT NULL,
+			password TEXT NOT NULL
+		);
+	`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("Error creating the user table: %v\n", err)
+	}
+}
+
+func saveUser(db *sql.DB, user User) error {
+	stmt, err := db.Prepare("INSERT INTO users(email, username, password) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Email, user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DecodeJSONBody(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	if r.Header.Get("Content-Type") != "application/json" {
+		return fmt.Errorf("the content type of the request body must be application/json")
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	err := decoder.Decode(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
