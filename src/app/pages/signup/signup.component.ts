bb00a35553ce639b49c4c701243df1d56198a5b9
L0G1C06 <justgithub70@gmail.com>
2024-04-12 14:01:01 -0300

    add verification if passwords are the same in signup forms

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -20,19 +20,33 @@ export class SignupComponent {
       email: [''],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    }, { validator: this.passwordMatchValidator
     });
   }
 
+  passwordMatchValidator(formGroup: FormGroup){
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword){
+      formGroup.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      formGroup.get('confirmPassword')?.setErrors(null);
+    }
+  }
+
   signup(): void {
-    if (this.userForm.valid){
+    if (this.userForm.valid) {
       console.log('New User');
       alert('User successfully registered!');
       this.router.navigate(['/login']);
     } else {
-      alert('Please fill all the fields correctly.');
+      const confirmPasswordControl = this.userForm.get('confirmPassword');
+      if (confirmPasswordControl?.hasError('mismatch')) {
+        alert('Passwords need to be the same!');
+      } else {
+        alert('Please fill all the fields correctly!');
+      }
     }
-  }
-
-  private async createNewUser(newUser: any): Promise<void> {
   }
 }
