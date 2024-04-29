d33ad88b6b47da54a3542f8488103b1f1c7d1fbe
L0G1C06 <justgithub70@gmail.com>
2024-04-29 17:51:58 -0300

    basics implemented with the backend

--- src/app/pages/home/components/welcome/welcome.component.ts
+++ src/app/pages/home/components/welcome/welcome.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface RoleCount {
+  data: { role: string; count: number; }[];
+  message: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -8,13 +13,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WelcomeComponent implements OnInit {
   totalUsers: number = 0;
-  userRoles: { role: string; count: number }[] = [];
+  userRoles: RoleCount = { data: [], message: '' };
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.fetchUserCount();
-    this.fetchUserRoles();
   }
 
   fetchUserCount(): void {
@@ -24,17 +28,6 @@ export class WelcomeComponent implements OnInit {
       },
       (error) => {
         console.error('Error fetching user count:', error);
-      }
-    );
-  }
-
-  fetchUserRoles(): void {
-    this.http.get<any>('http://0.0.0.0:8000/api/v1/users/list').subscribe(
-      (response) => {
-        this.userRoles = response.data;
-      },
-      (error) => {
-        console.error('Error fetching user roles:', error);
       }
     );
   }
