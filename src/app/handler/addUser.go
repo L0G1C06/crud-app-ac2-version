f98485c64237d7bc76988219a1d0446dc8a6d0db
L0G1C06 <justgithub70@gmail.com>
2024-04-30 10:06:38 -0300

    add hash in password for create user and add alerts to existent users in the databases

--- src/app/handler/addUser.go
+++ src/app/handler/addUser.go
@@ -3,12 +3,14 @@ package handler
 import (
 	"net/http"
 
+	"github.com/L0G1C06/crud-app/config"
 	"github.com/L0G1C06/crud-app/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func AddHandler(ctx *gin.Context){
 	var request CreateCrudRequest
+
 	if err := ctx.ShouldBindJSON(&request); err != nil{
 		SendError(ctx, http.StatusBadRequest, err.Error())
 		return 
@@ -20,10 +22,24 @@ func AddHandler(ctx *gin.Context){
 		return 
 	}
 
+	existingUser := schemas.Credentials{}
+	if err := db.Where("username = ?", request.Username).First(&existingUser).Error; err == nil {
+		SendError(ctx, http.StatusConflict, "username already exists")
+		return
+	}
+
+	// Hash the password
+	hashedPassword, err := config.HashPassword(request.Password)
+	if err != nil{
+		logger.Errorf("error hashing password: %v", err.Error())
+		SendError(ctx, http.StatusInternalServerError, "error hashing password")
+		return
+	}
+
 	addUser := schemas.Credentials{
 		Username: request.Username,
 		Email: request.Email,
-		Password: request.Password,
+		Password: hashedPassword,
 		Role: request.Role,
 	}
 
--- src/app/handler/login.go
+++ src/app/handler/login.go
@@ -4,14 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/L0G1C06/crud-app/config"
 	"github.com/L0G1C06/crud-app/schemas"
 )
-
-func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
-}
 
 func LoginHandler(ctx *gin.Context) {
 	var requestBody struct {
@@ -35,7 +30,7 @@ func LoginHandler(ctx *gin.Context) {
 		return
 	}
 
-	if !CheckPasswordHash(requestBody.Password, credentials.Password) {
+	if !config.CheckPasswordHash(requestBody.Password, credentials.Password) {
 		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "username or password are incorrect"})
 		return
 	}
