c649aa54cea46ab30b5bac39f5813d2c3d5ab936
L0G1C06 <justgithub70@gmail.com>
2024-06-03 13:57:59 -0300

    formatted code

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,25 +2,18 @@ import { Injectable, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
+@Injectable() export class AuthGuardService{
+  constructor(private cookieService: CookieService, public router: Router,) { }
 
-@Injectable()
- export class AuthGuardService {
-
- constructor(
- private cookieService: CookieService,
- public router: Router,
- ) { }
-
- canActivate(): boolean {
- if (this.cookieService.get('authToken')) {
-   return true
- } else {
-   this.router.navigate(['/login']);
-   return false
-   }
- }
-
- }
+  canActivate(): boolean {
+    if (this.cookieService.get('authToken')) {
+      return true
+    } else {
+      this.router.navigate(['/login'])
+      return false
+    }
+  }
+}
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
   return inject(AuthGuardService).canActivate();
